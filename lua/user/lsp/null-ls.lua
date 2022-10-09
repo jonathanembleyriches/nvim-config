6c9014f4de7a3e256a42ea5edcc2ff9a843fd780
jonathan <jonathanembleyriches@outlook.com>
2022-10-09 15:52:31 +0100

    Updated the formatter for python

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -12,13 +12,10 @@ local diagnostics = null_ls.builtins.diagnostics
 null_ls.setup {
   debug = false,
   sources = {
-    formatting.prettier.with {
-      extra_filetypes = { "toml","flake8" },
-      extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
-    },
-    formatting.black.with { extra_args = { "--fast" } },
-    formatting.stylua,
-    formatting.google_java_format,
+    formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+		formatting.black.with({ extra_args = { "--fast" } }),
+
     diagnostics.flake8,
+    formatting.black,
   },
 }
