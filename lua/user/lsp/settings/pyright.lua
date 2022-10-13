584f97f59d3a062535c50bc1edb5e3950d17f1c7
jonathan <jonathanembleyriches@outlook.com>
2022-10-13 11:01:21 +0100

    force update

--- lua/user/lsp/settings/pyright.lua
+++ lua/user/lsp/settings/pyright.lua
@@ -3,7 +3,11 @@ return {
     python = {
       analysis = {
         typeCheckingMode = "on",
+
       },
+      -- venvPath = "/home/jon/Documents/python_envs/",
+      -- venv = "env_1",
+      pythonPath = "/home/jon/Documents/python_envs/env_2/bin/python3",
     },
   },
 }
